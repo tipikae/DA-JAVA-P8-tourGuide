58b3aa326a944a3e8c0ca65459d14eedadb629dc
tipikae <gilles.bd@free.fr>
2022-03-08 16:00:04 +0100

    setting gpsService with discovery server

--- gpsService/src/main/java/com/tripmaster/tourguide/gpsService/GpsServiceApplication.java
+++ gpsService/src/main/java/com/tripmaster/tourguide/gpsService/GpsServiceApplication.java
@@ -6,6 +6,7 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * GpsUtil main class.
@@ -14,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GpsServiceApplication {
 	
 	@PostConstruct
