f0f5ff885574b75daf57dfc600faac967872b43a
tipikae <gilles.bd@free.fr>
2022-03-08 10:02:23 +0100

    renaming microservices

---
+++ gpsService/src/test/java/com/tripmaster/tourguide/gpsUtil/GpsUtilControllerTest.java
@@ -0,0 +1,14 @@
+package com.tripmaster.tourguide.gpsUtil;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GpsUtilControllerTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+
+}
---
+++ gpsService/src/test/java/com/tripmaster/tourguide/gpsUtil/GpsUtilServiceTest.java
@@ -0,0 +1,14 @@
+package com.tripmaster.tourguide.gpsUtil;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GpsUtilServiceTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+
+}
