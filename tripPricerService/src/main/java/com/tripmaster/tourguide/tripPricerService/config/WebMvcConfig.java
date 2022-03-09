dd709725b35de3593a8750e7f6ec74301965bfe4
tipikae <gilles.bd@free.fr>
2022-03-09 15:47:28 +0100

    adding config

---
+++ tripPricerService/src/main/java/com/tripmaster/tourguide/tripPricerService/config/WebMvcConfig.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.tripmaster.tourguide.tripPricerService.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Web MVC conbfiguration.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	private HandlerInterceptor requestLoggingInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(requestLoggingInterceptor);
+	}
+
+}
