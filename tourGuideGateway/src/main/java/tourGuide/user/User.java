a1c1107ae68197f9d0e4598484734bbda3740364
tipikae <gilles.bd@free.fr>
2022-03-10 17:55:54 +0100

    updating user model

--- tourGuideGateway/src/main/java/tourGuide/user/User.java
+++ tourGuideGateway/src/main/java/tourGuide/user/User.java
@@ -19,6 +19,7 @@ public class User {
 	private List<UserReward> userRewards = new ArrayList<>();
 	private UserPreferences userPreferences = new UserPreferences();
 	private List<Provider> tripDeals = new ArrayList<>();
+	
 	public User(UUID userId, String userName, String phoneNumber, String emailAddress) {
 		this.userId = userId;
 		this.userName = userName;
