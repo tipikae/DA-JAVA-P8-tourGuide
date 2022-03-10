0f08ad03d8277bc152bfea056bc807d3be741bbd
tipikae <gilles.bd@free.fr>
2022-03-10 10:30:35 +0100

    adding exceptions

---
+++ rewardsCentral/src/main/java/com/tripmaster/tourguide/rewardsCentral/exceptions/RewardCentralException.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.tripmaster.tourguide.rewardsCentral.exceptions;
+
+/**
+ * Parent exception for all RewardCentral exceptions.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+public class RewardCentralException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public RewardCentralException(String arg0) {
+		super(arg0);
+	}
+
+}
