3e99be829a5e97fcd227eead3511116d74454479
tipikae <gilles.bd@free.fr>
2022-03-10 17:35:09 +0100

    adding exceptions

---
+++ userService/src/main/java/com/tripmaster/tourguide/userService/exceptions/UserServiceException.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.tripmaster.tourguide.userService.exceptions;
+
+/**
+ * Parent exception for all UserService exceptions.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+public class UserServiceException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public UserServiceException(String arg0) {
+		super(arg0);
+	}
+
+}
