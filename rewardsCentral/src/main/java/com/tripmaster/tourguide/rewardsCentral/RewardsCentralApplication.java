7bdc598072c94d6a6365566284f0e9315f4321c2
tipikae <gilles.bd@free.fr>
2022-03-10 10:17:36 +0100

    adding rewardsCentral service

---
+++ rewardsCentral/src/main/java/com/tripmaster/tourguide/rewardsCentral/RewardsCentralApplication.java
@@ -0,0 +1,13 @@
+package com.tripmaster.tourguide.rewardsCentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsCentralApplication.class, args);
+	}
+
+}
