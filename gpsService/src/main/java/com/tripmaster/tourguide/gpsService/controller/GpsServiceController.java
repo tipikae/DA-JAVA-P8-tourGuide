9ce1e9c32fe3bbdb7b3b057e1b5f28ced099a1ce
tipikae <gilles.bd@free.fr>
2022-03-14 19:32:41 +0100

    updating controller and tests

--- gpsService/src/main/java/com/tripmaster/tourguide/gpsService/controller/GpsServiceController.java
+++ gpsService/src/main/java/com/tripmaster/tourguide/gpsService/controller/GpsServiceController.java
@@ -4,6 +4,7 @@
 package com.tripmaster.tourguide.gpsService.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.slf4j.Logger;
@@ -13,13 +14,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tripmaster.tourguide.gpsService.exceptions.UserNotFoundException;
 import com.tripmaster.tourguide.gpsService.service.IGpsServiceService;
 
 import gpsUtil.location.Attraction;
+import gpsUtil.location.Location;
 import gpsUtil.location.VisitedLocation;
 
 /**
@@ -35,21 +40,80 @@ public class GpsServiceController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(GpsServiceController.class);
 	
 	@Autowired
-	private IGpsServiceService gpsUtilService;
+	private IGpsServiceService gpsService;
 	
+	/**
+	 * Get all attractions.
+	 * @return ResponseEntity<List<Attraction>>
+	 */
 	@GetMapping("/attractions")
-	public ResponseEntity<Object> getAttractions() throws Exception {
+	public ResponseEntity<Object> getAttractions() {
 		LOGGER.info("getAttractions");
-		List<Attraction> attractions = gpsUtilService.getAttractions();
+		List<Attraction> attractions = gpsService.getAttractions();
 		return new ResponseEntity<Object>(attractions, HttpStatus.OK);
 	}
 	
-	@GetMapping("/userlocation")
-	public ResponseEntity<Object> getUserLocation(@RequestParam @NonNull UUID userId) 
-			throws Exception {
+	/**
+	 * Get a user's location.
+	 * @param userId UUID
+	 * @return ResponseEntity<VisitedLocation>
+	 */
+	@GetMapping("/location")
+	public ResponseEntity<Object> getUserLocation(@RequestParam @NonNull UUID userId) {
 		LOGGER.info("getUserLocation: userId=" + userId);
-		VisitedLocation visitedLocation = gpsUtilService.getUserLocation(userId);
+		VisitedLocation visitedLocation = gpsService.getUserLocation(userId);
 		return new ResponseEntity<Object>(visitedLocation, HttpStatus.OK);
+	}
+	
+	/**
+	 * Get all users' last location.
+	 * @return ResponseEntity<Map<UUID, Location>>
+	 */
+	@GetMapping("/lastlocations")
+	public ResponseEntity<Object> getAllUsersLastLocation() {
+		LOGGER.info("getAllUsersLastLocation");
+		Map<UUID, Location> allUsersLastLocation = gpsService.getAllUsersLastLocation();
+		return new ResponseEntity<Object>(allUsersLastLocation, HttpStatus.OK);
+	}
+	
+	/**
+	 * Get a user's all visited locations.
+	 * @param userId UUID
+	 * @return ResponseEntity<List<VisitedLocation>>
+	 * @throws UserNotFoundException 
+	 */
+	@GetMapping("/locations")
+	public ResponseEntity<Object> getUserVisitedLocations(@RequestParam @NonNull UUID userId) 
+			throws UserNotFoundException {
+		LOGGER.info("getUserVisitedLocations: userId=" + userId);
+		List<VisitedLocation> visitedLocations = gpsService.getUserVisitedLocations(userId);
+		return new ResponseEntity<Object>(visitedLocations, HttpStatus.OK);
+	}
+	
+	/**
+	 * Get a user's last location.
+	 * @param userId UUID
+	 * @return ResponseEntity<VisitedLocation>
+	 * @throws UserNotFoundException 
+	 */
+	@GetMapping("/lastlocation")
+	public ResponseEntity<Object> getUserLastVisitedLocation(@RequestParam @NonNull UUID userId)
+			throws UserNotFoundException {
+		LOGGER.info("getUserLastVisitedLocation: userId=" + userId);
+		VisitedLocation visitedLocation = gpsService.getUserLastVisitedLocation(userId);
+		return new ResponseEntity<Object>(visitedLocation, HttpStatus.OK);
+	}
+	
+	/**
+	 * Add a users's visitedLocation.
+	 * @param visitedLocation
+	 * @return ResponseEntity<VisitedLocation>
+	 */
+	@PostMapping(value = "/location", consumes={"application/json"})
+	public ResponseEntity<Object> addUserVisitedLocation(@RequestBody VisitedLocation visitedLocation) {
+		LOGGER.info("addUserVisitedLocation");
+		VisitedLocation response = gpsService.addUserVisitedLocation(visitedLocation);
+		return new ResponseEntity<Object>(response, HttpStatus.OK);
 	}
 
 }
--- gpsService/src/test/java/com/tripmaster/tourguide/gpsService/GpsServiceControllerTest.java
+++ gpsService/src/test/java/com/tripmaster/tourguide/gpsService/GpsServiceControllerTest.java
@@ -4,14 +4,18 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -20,6 +24,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.tripmaster.tourguide.gpsService.controller.GpsServiceController;
+import com.tripmaster.tourguide.gpsService.exceptions.UserNotFoundException;
 import com.tripmaster.tourguide.gpsService.service.IGpsServiceService;
 
 import gpsUtil.location.Attraction;
@@ -34,6 +39,23 @@ class GpsServiceControllerTest {
 	
 	@MockBean
 	private IGpsServiceService gpsService;
+	
+	private static UUID userId;
+	private static Location location;
+	private static VisitedLocation visitedLocation;
+	private static List<VisitedLocation> visitedLocations;
+	private static Map<UUID, List<VisitedLocation>> map;
+	
+	@BeforeAll
+	private static void setUp() {
+		userId = UUID.randomUUID();
+		location = new Location(10d, 20d);
+		visitedLocation = new VisitedLocation(userId, location, new Date());
+		visitedLocations = new ArrayList<>();
+		visitedLocations.add(visitedLocation);
+		map = new HashMap<>();
+		map.put(userId, visitedLocations);
+	}
 
 	@Test
 	void getAttractionsReturnsJsonListWhenOk() throws Exception {
@@ -49,14 +71,50 @@ class GpsServiceControllerTest {
 	
 	@Test
 	void getUserLocationReturnsLocationWhenOk() throws Exception {
-		UUID userId = UUID.fromString("019b04a9-067a-4c76-8817-ee75088c3822");
-		Location location = new Location(-110.464573, -25.075681);
-		Date now = new Date();
-		VisitedLocation visitedLocation = new VisitedLocation(userId, location, now);
 		when(gpsService.getUserLocation(any(UUID.class))).thenReturn(visitedLocation);
-		mockMvc.perform(get("/gpsservice/userlocation")
+		mockMvc.perform(get("/gpsservice/location")
 				.contentType(MediaType.APPLICATION_JSON)
 				.param("userId", userId.toString()))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.userId", is(userId.toString())));
+	}
+	
+	@Test
+	void getAllUsersLastLocationReturnsMapWhenOk() throws Exception {
+		Map<UUID, Location> res = new HashMap<>();
+		res.put(userId, location);
+		when(gpsService.getAllUsersLastLocation()).thenReturn(res);
+		mockMvc.perform(get("/gpsservice/lastlocations"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$." + userId.toString() + ".latitude", is(10d)));
+	}
+	
+	@Test
+	void getUserVisitedLocationsReturnsListWhenOk() throws UserNotFoundException, Exception {
+		when(gpsService.getUserVisitedLocations(any(UUID.class))).thenReturn(visitedLocations);
+		mockMvc.perform(get("/gpsservice/locations")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("userId", userId.toString()))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.[0].location.longitude", is(20d)));
+	}
+	
+	@Test
+	void getUserLastVisitedLocationReturnsVisitedLocationWhenOk() throws UserNotFoundException, Exception {
+		when(gpsService.getUserLastVisitedLocation(any(UUID.class))).thenReturn(visitedLocation);
+		mockMvc.perform(get("/gpsservice/lastlocation")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("userId", userId.toString()))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.userId", is(userId.toString())));
+	}
+	
+	@Test
+	void addUserVisitedLocationReturnsVisitedLocationWhenOk() throws Exception {
+		when(gpsService.addUserVisitedLocation(any(VisitedLocation.class))).thenReturn(visitedLocation);
+		mockMvc.perform(post("/gpsservice/location")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"userId\":\"776aae36-5afb-44e8-a357-c4f20242eb41\",\"location\":{\"longitude\":20.0,\"latitude\":10.0},\"timeVisited\":\"2022-03-14T18:01:01.846+00:00\"}"))
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.userId", is(userId.toString())));
 	}
