963e04d97c08bbea8cc6f11ec0454218ad7f21dc
tipikae <gilles.bd@free.fr>
2022-03-16 10:34:50 +0100

    adding exception

--- userService/src/main/java/com/tripmaster/tourguide/userService/exceptions/UserAlreadyExistsException.java
+++ userService/src/main/java/com/tripmaster/tourguide/userService/exceptions/UserAlreadyExistsException.java
@@ -6,7 +6,7 @@ package com.tripmaster.tourguide.userService.exceptions;
 /**
  * User already exists exception.
  * @author tipikae
- * @version 1.0
+ *  @version 1.0
  *
  */
 public class UserAlreadyExistsException extends UserServiceException {
